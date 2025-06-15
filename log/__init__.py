805454ecf912ce384ca78057cdfb7742e56c6fe8
Khiet_Tuong <khiettuongcat460@gmail.com>
2025-06-15 09:29:58 +0700

    update

--- log/__init__.py
+++ log/__init__.py
@@ -1,1 +1,11 @@
-from .log import *
+from .logs import Log
+
+error = Log.error
+info = Log.info
+warn = Log.warn
+success = Log.success
+debug = Log.debug
+log_file = Log.log_file
+log_custom = Log.log_custom
+
+__all__ = ['error', 'info', 'warn', 'success', 'debug', 'log_file', 'log_custom']
